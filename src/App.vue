9b318a90c41616092682a9b5552a66fffbf13c4a
SirAlyon <alessiosada04@gmail.com>
2022-05-09 17:14:22 +0200

    add dynamic stars

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,73 @@
   <div id="app">
     <SiteHeader />
     <!-- <SiteMain /> -->
-    <div class="films" v-if="!loading">
-      <div class="film" v-for="(film, index) in films" :key="index">
-        <div class="title">{{ film.title }}</div>
-        <div class="original_title">{{ film.original_title }}</div>
-        <img :src="getImage(film.backdrop_path)" alt=""/>
+    <div class="container">
+      <div class="row">
+        <div class="films" v-if="!loading">
+          <div class="col-2" v-for="(film, index) in films" :key="index">
+            <div class="film">
+              <div class="title">{{ film.title }}</div>
+              <div class="original_title">{{ film.original_title }}</div>
+              <img :src="getImage(film.backdrop_path)" alt="" />
 
-        <div class="lang">
-          {{ film.original_language }}
-          <country-flag
-            :country="flags(film.original_language)"
-            size="normal"
-          />
+              <div class="lang">
+                {{ film.original_language }}
+                <country-flag
+                  :country="flags(film.original_language)"
+                  size="normal"
+                />
+              </div>
+              <div class="review">{{ getVote(film.vote_average) }}</div>
+              <div class="star">
+                <span class="voted"
+                  v-for="s in getVote(film.vote_average)"
+                  :key="'star-n' + s">
+                  <font-awesome-icon icon="fa-solid fa-star"/>
+                </span>
+
+                <span>
+                  <font-awesome-icon
+                    icon="fa-solid fa-star"
+                    v-for="s in 5 - getVote(film.vote_average)"
+                    :key="'star-n' + s"
+                  />
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="review">{{ film.vote_average }}</div>
-      </div>
-    </div>
-    <div class="series" v-if="!loading">
-      <div class="serie" v-for="(serie, index) in series" :key="index">
-        <div class="title">{{ serie.name }}</div>
-        <div class="original_title">{{ serie.original_name }}</div>
-        <img
-          :src="getImage(serie.backdrop_path)"
-          alt=""
-        />
-        <div class="lang">
-          {{ serie.original_language }}
-          <country-flag
-            :country="flags(serie.original_language)"
-            size="normal"
-          />
+        <div class="series" v-if="!loading">
+          <div class="col-2" v-for="(serie, index) in series" :key="index">
+            <div class="serie">
+              <div class="title">{{ serie.name }}</div>
+              <div class="original_title">{{ serie.original_name }}</div>
+              <img :src="getImage(serie.backdrop_path)" alt="" />
+              <div class="lang">
+                {{ serie.original_language }}
+                <country-flag
+                  :country="flags(serie.original_language)"
+                  size="normal"
+                />
+              </div>
+              <div class="review">{{ getVote(serie.vote_average) }}</div>
+              <div class="star">
+                <span class="voted"
+                  v-for="s in getVote(serie.vote_average)"
+                  :key="'star-n' + s">
+                  <font-awesome-icon icon="fa-solid fa-star"/>
+                </span>
+
+                <span>
+                  <font-awesome-icon
+                    icon="fa-solid fa-star"
+                    v-for="s in 5 - getVote(serie.vote_average)"
+                    :key="'star-n' + s"
+                  />
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="review">{{ serie.vote_average }}</div>
       </div>
     </div>
   </div>
@@ -70,18 +105,25 @@ export default {
         return lang;
       }
     },
-    getImage(thumb){
-      if (thumb !== '' && thumb !== null){
-        return 'https://image.tmdb.org/t/p/w300/' + thumb
+    getImage(thumb) {
+      if (thumb !== "" && thumb !== null) {
+        return "https://image.tmdb.org/t/p/w300/" + thumb;
       } else {
-        return 'https://picsum.photos/300/170'
+        return "https://picsum.photos/300/170";
       }
-    }
+    },
+    getVote(vote) {
+      return Math.ceil(vote / 2);
+    },
+    getStars() {
+      return '<font-awesome-icon icon="fa-solid fa-star" />';
+    },
   },
 };
 </script>
 
 <style lang="scss">
+@import "~/node_modules/bootstrap/scss/bootstrap.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -94,16 +136,29 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+
+img {
+  max-width: 100%;
+}
+
+.voted {
+  color: gold;
+}
+
 .films,
 .series {
   display: flex;
+  flex-wrap: wrap;
   .film,
   .serie {
     padding: 1rem;
     border: 1px solid black;
+    min-height: 350px;
     .title {
       color: blue;
       margin-bottom: 2rem;
+      height: 50px;
+      overflow: hidden;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+/* Font Awesome icons*/
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons 👇 QUI AGGIUNGI LE ICONE che vuoi usare in camelCase */
+import { faUserSecret, faStar } from '@fortawesome/free-solid-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library 👇 QUI AGGIUNGI IL NOME DELL'ICONA in camelCase*/
+library.add(faUserSecret, faStar) 
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+const bootstrap = require('bootstrap')
+Vue.use(bootstrap)
+
 Vue.config.productionTip = false
 
 new Vue({
