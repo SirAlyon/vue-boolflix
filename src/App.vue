10b73c0cd565a156c66b58d8df602247e2d0e386
SirAlyon <alessiosada04@gmail.com>
2022-05-09 12:25:59 +0200

    psuh for ticket

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <SiteHeader />
-
+    <SiteMain />
   </div>
 </template>
 
 <script>
 import SiteHeader from '@/components/SiteHeaderComponent.vue'
+import SiteMain from '@/components/SiteMainComponent.vue'
 
 export default {
   name: 'App',
   components: {
     SiteHeader,
+    SiteMain
   }
 }
 </script>
